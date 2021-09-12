6dee57146df078f6e64d1b7413dfbc49c29687dc
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2021-09-12 13:09:12 -0400

    logout test initiated

---
+++ tests/test_logout.py
@@ -0,0 +1,17 @@
+"""
+A Test for logout function. User should be able to log out
+"""
+
+
+def test_logout(browser):
+
+    # Given user logged in and secure area page is displayed
+    # TODO
+
+    # When the user click the "Logout" button
+    # TODO
+
+    # Then the user redirected to the login page and message "You logged out of the secure area!" appear.
+    # TODO
+
+    raise Exception("Incomplete Test")
