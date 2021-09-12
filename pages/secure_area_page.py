36e271e6879f2276aa664922d4906a93ff072624
Aleksandr Kuzmin <alex.kuzminn@gmail.com>
2021-09-12 16:13:00 -0400

    log out test completed

--- pages/secure_area_page.py
+++ pages/secure_area_page.py
@@ -9,6 +9,8 @@ class SecureAreaPage:
 
     # Locators
     WELCOME_PHRASE = (By.XPATH, "//h4[contains(text(),'Welcome to the Secure Area. When you are done clic')]")
+    LOGIN_PAGE = (By.XPATH, "//h2[contains(text(),'Login Page')]")
+    LOGOUT_BUTTON = (By.XPATH, "//i[contains(text(),'Logout')]")
 
     # Initializer
     def __init__(self, browser):
@@ -18,4 +20,13 @@ class SecureAreaPage:
     def welcome_phrase(self):
         page_phrase = self.browser.find_element(*self.WELCOME_PHRASE)
         text = page_phrase.text
+        return text
+
+    def logout_button(self):
+        button = self.browser.find_element(*self.LOGOUT_BUTTON)
+        button.click()
+
+    def login_page_phrase(self):
+        phrase = self.browser.find_element(*self.LOGIN_PAGE)
+        text = phrase.text
         return text
--- tests/test_logout.py
+++ tests/test_logout.py
@@ -1,17 +1,27 @@
 """
 A Test for logout function. User should be able to log out
 """
+from pages.login_page import TheInternetLoginPage
+from pages.secure_area_page import SecureAreaPage
 
 
 def test_logout(browser):
+    login_page = TheInternetLoginPage(browser)
+    secure_page = SecureAreaPage(browser)
+    USER_NAME = "tomsmith"
+    PASSWORD = "SuperSecretPassword!"
+    PHRASE = "Welcome to the Secure Area. When you are done click logout below."
+    LOGIN_PHRASE = "Login Page"
 
     # Given user logged in and secure area page is displayed
-    # TODO
+    login_page.load()
+    login_page.username(USER_NAME)
+    login_page.password(PASSWORD)
+    login_page.login_button()
+    assert PHRASE == secure_page.welcome_phrase()
 
     # When the user click the "Logout" button
-    # TODO
+    secure_page.logout_button()
 
     # Then the user redirected to the login page and message "You logged out of the secure area!" appear.
-    # TODO
-
-    raise Exception("Incomplete Test")
+    assert LOGIN_PHRASE == secure_page.login_page_phrase()
